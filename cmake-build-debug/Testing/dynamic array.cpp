dceba5df89bb2ca11e3dc1c0c222164a784a1a3d
trismagostis <piotr.kaczmarzyk@poczta.fm>
2022-01-13 11:37:21 +0100

    xx

--- cmake-build-debug/Testing/dynamic array.cpp
+++ cmake-build-debug/Testing/dynamic array.cpp
@@ -1,4 +1,25 @@
-//
-// Created by ZST on 1/13/2022.
-//
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+
+
+int main() {
+    int n;
+    cout << "podaj rozmiar tablicy\n";
+    cin >> n;
+    double  *wsk;
+    wsk = new double[n];
+    for (int i =0; i < n; i++) {
+        cout << "podaj wartosc " << i << endl;
+        cin >> *(wsk + i);
+    }
+    double suma {0};
+
+    for (int i =0; i < n; i++) {
+        suma += *(wsk +i);
+    }
+    delete[] wsk;
+    cout << "suma: " << suma << endl;
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main()
-{
-    int *wsk;
-    wsk=new int;
-    *wsk=5;
-    cout<<*wsk;
-    delete wsk;
-        return 0;
+
+
+int main() {
+    int n;
+    cout << "podaj rozmiar tablicy\n";
+    cin >> n;
+    double  *wsk;
+    wsk = new double[n]; //ustalam rozmiar tablicy
+    for (int i =0; i < n; i++) {
+        cout << "podaj wartosc " << i << endl;
+        cin >> *(wsk + i);
+    }
+    double suma {0};
+
+    for (int i =0; i < n; i++) {
+        suma += *(wsk +i);
+    }
+    cout << (wsk+1);
+    *(wsk+n)=5; //chce wstawic element za ostatnim elementem tablicy (nie mogę już zmienić ustalonego rozmiaru)
+    delete[] wsk;
+    cout << "suma: " << suma << endl;
+
+
+    return 0;
 }
 
