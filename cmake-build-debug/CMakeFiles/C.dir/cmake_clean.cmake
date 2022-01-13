1c085e6250051bc938ecb4cc285d5371600b038e
trismagostis <piotr.kaczmarzyk@poczta.fm>
2022-01-13 10:49:44 +0100

    xx

---
+++ cmake-build-debug/CMakeFiles/C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "C.exe"
+  "C.exe.manifest"
+  "C.lib"
+  "C.pdb"
+  "CMakeFiles/C.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
