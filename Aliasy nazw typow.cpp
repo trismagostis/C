4eb4f58228e9ac7424ac1a13874e414b904b45e2
trismagostis <piotr.kaczmarzyk@poczta.fm>
2022-01-24 12:57:28 +0100

    xx

--- Aliasy nazw typow.cpp
+++ Aliasy nazw typow.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+typedef int calkowita;
+using rzeczywiste=double;
 int main()
 {
     int a =5;
@@ -9,4 +11,8 @@ int main()
     auto g='d';
     cout << g;
     return 0;
+    calkowita a =5;
+    rzeczywiste b =4.6;
+    cout << b;
+
 }
